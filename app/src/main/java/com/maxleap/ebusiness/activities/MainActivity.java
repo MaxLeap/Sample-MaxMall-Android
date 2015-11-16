0619e470e872f17b0a665a9a5c76541c134fac14
Reacoder <sampythoner@gmail.com>
2015-11-16 15:59:42 +0800

    first commit.

---
+++ app/src/main/java/com/maxleap/ebusiness/activities/MainActivity.java
@@ -0,0 +1,55 @@
+package com.maxleap.ebusiness.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.maxleap.ebusiness.R;
+import com.maxleap.ebusiness.fragments.CategoryFragment;
+import com.maxleap.ebusiness.fragments.MainFragment;
+import com.maxleap.ebusiness.fragments.MineFragment;
+import com.maxleap.ebusiness.fragments.ShopFragment;
+
+public class MainActivity extends BaseActivity {
+
+    private FragmentTabHost mTabHost;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        initTab();
+    }
+
+    private void initTab() {
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        mTabHost.getTabWidget().setDividerDrawable(null);
+        mTabHost.addTab(mTabHost.newTabSpec("mainTab").setIndicator(getTabView(R.mipmap.ic_launcher, R.string.activity_main_tab_home)),
+                MainFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("categoryTab").setIndicator(getTabView(R.mipmap.ic_launcher, R.string.activity_main_tab_category)),
+                CategoryFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("shopTab").setIndicator(getTabView(R.mipmap.ic_launcher, R.string.activity_main_tab_shop)),
+                ShopFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("mineTab").setIndicator(getTabView(R.mipmap.ic_launcher, R.string.activity_main_tab_mine)),
+                MineFragment.class, null);
+    }
+
+    private View getTabView(int imgId, int txtId) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.view_home_tab, null);
+        ((ImageView) view.findViewById(R.id.tab_img)).setImageResource(imgId);
+        ((TextView) view.findViewById(R.id.tab_label)).setText(txtId);
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/maxleap/ebusiness/fragments/ShopFragment.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015-present, MaxLeapMobile.
+ * All rights reserved.
+ * ----
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.maxleap.ebusiness.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ShopFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("ShopFragment");
+        textView.setTextColor(Color.BLUE);
+        textView.setTextSize(22);
+        return textView;
+    }
+}
