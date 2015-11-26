48c86eb35520ae7050ce5b578eb3ced201799dbb
Reacoder <sampythoner@gmail.com>
2015-11-26 11:51:56 +0800

    build config done.

--- app/src/main/java/com/maxleap/ebusiness/activities/BaseActivity.java
+++ app/src/main/java/com/maxleap/ebusiness/activities/BaseActivity.java
@@ -11,10 +11,28 @@ package com.maxleap.ebusiness.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.maxleap.MLAnalytics;
+import com.maxleap.ebusiness.ApiKey;
+
+import net.hockeyapp.android.CrashManager;
+
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MLAnalytics.onResume(this);
+        CrashManager.register(this, ApiKey.HOCKEY_APP_ID);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MLAnalytics.onPause(this);
     }
 }
